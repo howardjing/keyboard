b871b72d6d5a746486057e38cf442bebd76745f8
Howard Jing <howard.jing@gmail.com>
2017-07-23 18:29:02 -0400

    add react router

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
+import styled, { injectGlobal } from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import Editor from './keycap-editor';
-import { injectGlobal } from 'styled-components';
 
 injectGlobal`
   body {
     font-family: 'Roboto', sans-serif;
+    margin: 0;
   }
 `;
 
 const App = ({
 }) => (
-  <div>
-    <h3>Keycap Renderer</h3>
-    <Editor />
-  </div>
+  <Router>
+    <div>
+      <Nav />
+      <Content>
+        <Route exact path="/" component={Editor} />
+      </Content>
+    </div>
+  </Router>
 );
+
+const Nav = styled.nav`
+  box-sizing: border-box;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  height: 40px;
+  border-bottom: 1px solid #eee;
+`;
+
+const Content = styled.div`
+  margin: 10px;
+`;
 
 export default App;
