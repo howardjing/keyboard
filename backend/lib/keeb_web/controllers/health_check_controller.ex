43d7f3f15ca041b8aea2b5728d2e08f0c3ebd268
Howard Jing <howard.jing@gmail.com>
2017-08-06 17:46:37 -0400

    bump version

--- backend/lib/keeb_web/controllers/health_check_controller.ex
+++ backend/lib/keeb_web/controllers/health_check_controller.ex
@@ -2,6 +2,6 @@ defmodule KeebWeb.HealthCheckController do
   use KeebWeb, :controller
 
   def index(conn, _params) do
-    json conn, %{status: "ok"}
+    json conn, %{ version: Mix.Project.config[:version] }
   end
 end
