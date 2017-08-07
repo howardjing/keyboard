c27b0f96fa52f866749b2f8258de74c14c78b10a
Howard Jing <howard.jing@gmail.com>
2017-08-06 20:14:23 -0400

    add relatively unique label to keycaps

--- frontend/src/domains/keycap-editor/keyboard.ts
+++ frontend/src/domains/keycap-editor/keyboard.ts
@@ -8,100 +8,100 @@ const id = () => {
   return _id;
 }
 
-const ESC = () => Keycap.build('Esc');
-const F1 = () => Keycap.build('F1');
-const F2 = () => Keycap.build('F2');
-const F3 = () => Keycap.build('F3');
-const F4 = () => Keycap.build('F4');
-const F5 = () => Keycap.build('F5');
-const F6 = () => Keycap.build('F6');
-const F7 = () => Keycap.build('F7');
-const F8 = () => Keycap.build('F8');
-const F9 = () => Keycap.build('F9');
-const F10 = () => Keycap.build('F10');
-const F11 = () => Keycap.build('F11');
-const F12 = () => Keycap.build('F12');
-const PRINT_SCREEN = () => Keycap.build('PrtSc');
-const SCROLL_LOCK = () => Keycap.build('ScrlLk');
-const PAUSE = () => Keycap.build('Pause');
+const ESC = () => Keycap.build('ESC', 'Esc');
+const F1 = () => Keycap.build('F1', 'F1');
+const F2 = () => Keycap.build('F2', 'F2');
+const F3 = () => Keycap.build('F3', 'F3');
+const F4 = () => Keycap.build('F4', 'F4');
+const F5 = () => Keycap.build('F5', 'F5');
+const F6 = () => Keycap.build('F6', 'F6');
+const F7 = () => Keycap.build('F7', 'F7');
+const F8 = () => Keycap.build('F8', 'F8');
+const F9 = () => Keycap.build('F9', 'F9');
+const F10 = () => Keycap.build('F10', 'F10');
+const F11 = () => Keycap.build('F11', 'F11');
+const F12 = () => Keycap.build('F12', 'F12');
+const PRINT_SCREEN = () => Keycap.build('PRINT_SCREEN', 'PrtSc');
+const SCROLL_LOCK = () => Keycap.build('SCROLL_LOCK', 'ScrlLk');
+const PAUSE = () => Keycap.build('PAUSE', 'Pause');
 
-const BACKTICK = () => Keycap.build('`', '~');
-const ONE = () => Keycap.build('1', '!');
-const TWO = () => Keycap.build('2', '@');
-const THREE = () => Keycap.build('3', '#');
-const FOUR = () => Keycap.build('4', '$');
-const FIVE = () => Keycap.build('5', '%');
-const SIX = () => Keycap.build('6', '^');
-const SEVEN = () => Keycap.build('7', '&');
-const EIGHT = () => Keycap.build('8', '*');
-const NINE = () => Keycap.build('9', '(');
-const ZERO = () => Keycap.build('0', ')');
-const HYPHEN = () => Keycap.build('-', '_');
-const EQUAL = () => Keycap.build('=', '+');
-const BACKSPACE = () => Keycap.build('Backspace', '', 2);
+const BACKTICK = () => Keycap.build('BACKTICK', '`', '~');
+const ONE = () => Keycap.build('ONE', '1', '!');
+const TWO = () => Keycap.build('TWO', '2', '@');
+const THREE = () => Keycap.build('THREE', '3', '#');
+const FOUR = () => Keycap.build('FOUR', '4', '$');
+const FIVE = () => Keycap.build('FIVE', '5', '%');
+const SIX = () => Keycap.build('SIX', '6', '^');
+const SEVEN = () => Keycap.build('SEVEN', '7', '&');
+const EIGHT = () => Keycap.build('EIGHT', '8', '*');
+const NINE = () => Keycap.build('NINE', '9', '(');
+const ZERO = () => Keycap.build('ZERO', '0', ')');
+const HYPHEN = () => Keycap.build('HYPHEN', '-', '_');
+const EQUAL = () => Keycap.build('EQUAL', '=', '+');
+const BACKSPACE = () => Keycap.build('BACKSPACE','Backspace', '', 2);
 
-const TAB = () => Keycap.build('Tab', '', 1.5);
-const Q = () => Keycap.build('Q');
-const W = () => Keycap.build('W');
-const E = () => Keycap.build('E');
-const R = () => Keycap.build('R');
-const T = () => Keycap.build('T');
-const Y = () => Keycap.build('Y');
-const U = () => Keycap.build('U');
-const I = () => Keycap.build('I');
-const O = () => Keycap.build('O');
-const P = () => Keycap.build('P');
-const LEFT_SQUARE_BRACKET = () => Keycap.build('[', '{');
-const RIGHT_SQUARE_BRACKET = () => Keycap.build(']', '}');
-const BACKSLASH = () => Keycap.build('\\', '|', 1.5);
+const TAB = () => Keycap.build('TAB', 'Tab', '', 1.5);
+const Q = () => Keycap.build('Q', 'Q');
+const W = () => Keycap.build('W', 'W');
+const E = () => Keycap.build('E', 'E');
+const R = () => Keycap.build('R', 'R');
+const T = () => Keycap.build('T', 'T');
+const Y = () => Keycap.build('Y', 'Y');
+const U = () => Keycap.build('U', 'U');
+const I = () => Keycap.build('I', 'I');
+const O = () => Keycap.build('O', 'O');
+const P = () => Keycap.build('P', 'P');
+const LEFT_SQUARE_BRACKET = () => Keycap.build('LEFT_SQUARE_BRACKET', '[', '{');
+const RIGHT_SQUARE_BRACKET = () => Keycap.build('RIGHT_SQUARE_BRACKET', ']', '}');
+const BACKSLASH = () => Keycap.build('BACKSLASH', '\\', '|', 1.5);
 
-const CAPSLOCK = () => Keycap.build('Caps Lock', '', 1.75);
-const A = () => Keycap.build('A');
-const S = () => Keycap.build('S');
-const D = () => Keycap.build('D');
-const F = () => Keycap.build('F');
-const G = () => Keycap.build('G');
-const H = () => Keycap.build('H');
-const J = () => Keycap.build('J');
-const K = () => Keycap.build('K');
-const L = () => Keycap.build('L');
-const SEMI_COLON = () => Keycap.build(';', ':');
-const SINGLE_QUOTE = () => Keycap.build("'", '"');
-const ENTER = () => Keycap.build('Enter', '', 2.25);
+const CAPS_LOCK = () => Keycap.build('CAPS_LOCK', 'Caps Lock', '', 1.75);
+const A = () => Keycap.build('A', 'A');
+const S = () => Keycap.build('S', 'S');
+const D = () => Keycap.build('D', 'D');
+const F = () => Keycap.build('F', 'F');
+const G = () => Keycap.build('G', 'G');
+const H = () => Keycap.build('H', 'H');
+const J = () => Keycap.build('J', 'J');
+const K = () => Keycap.build('K', 'K');
+const L = () => Keycap.build('L', 'L');
+const SEMI_COLON = () => Keycap.build('SEMI_COLON', ';', ':');
+const SINGLE_QUOTE = () => Keycap.build('SINGLE_QUOTE', "'", '"');
+const ENTER = () => Keycap.build('ENTER', 'Enter', '', 2.25);
 
-const LEFT_SHIFT = () => Keycap.build('Shift', '', 2.25);
-const Z = () => Keycap.build('Z');
-const X = () => Keycap.build('X');
-const C = () => Keycap.build('C');
-const V = () => Keycap.build('V');
-const B = () => Keycap.build('B');
-const N = () => Keycap.build('N');
-const M = () => Keycap.build('M');
-const COMMA = () => Keycap.build(',', '<');
-const PERIOD = () => Keycap.build('.', '>');
-const FORWARD_SLASH = () => Keycap.build('/', '?');
-const RIGHT_SHIFT = () => Keycap.build('Shift', '', 2.75);
+const LEFT_SHIFT = () => Keycap.build('LEFT_SHIFT', 'Shift', '', 2.25);
+const Z = () => Keycap.build('Z', 'Z');
+const X = () => Keycap.build('X', 'X');
+const C = () => Keycap.build('C', 'C');
+const V = () => Keycap.build('V', 'V');
+const B = () => Keycap.build('B', 'B');
+const N = () => Keycap.build('N', 'N');
+const M = () => Keycap.build('M', 'M');
+const COMMA = () => Keycap.build('COMMA', ',', '<');
+const PERIOD = () => Keycap.build('PERIOD', '.', '>');
+const FORWARD_SLASH = () => Keycap.build('FORWARD_SLASH', '/', '?');
+const RIGHT_SHIFT = () => Keycap.build('RIGHT_SHIFT', 'Shift', '', 2.75);
 
-const LEFT_CONTROL = () => Keycap.build('Ctrl', '', 1.25);
-const LEFT_SUPER = () => Keycap.build('Super', '', 1.25);
-const LEFT_ALT = () => Keycap.build('Alt', '', 1.25);
-const SPACEBAR = () => Keycap.build('', '', 6.25, undefined, undefined, true);
-const RIGHT_ALT = () => Keycap.build('Alt', '', 1.25);
-const RIGHT_SUPER = () => Keycap.build('Super', '', 1.25);
-const MENU = () => Keycap.build('Menu', '', 1.25);
-const RIGHT_CONTROL = () => Keycap.build('Ctrl', '', 1.25);
+const LEFT_CONTROL = () => Keycap.build('LEFT_CONTROL', 'Ctrl', '', 1.25);
+const LEFT_SUPER = () => Keycap.build('LEFT_SUPER', 'Super', '', 1.25);
+const LEFT_ALT = () => Keycap.build('LEFT_ALT', 'Alt', '', 1.25);
+const SPACEBAR = () => Keycap.build('SPACEBAR', '', '', 6.25, undefined, undefined, true);
+const RIGHT_ALT = () => Keycap.build('RIGHT_ALT', 'Alt', '', 1.25);
+const RIGHT_SUPER = () => Keycap.build('RIGHT_SUPER', 'Super', '', 1.25);
+const MENU = () => Keycap.build('MENU', 'Menu', '', 1.25);
+const RIGHT_CONTROL = () => Keycap.build('RIGHT_CONTROL', 'Ctrl', '', 1.25);
 
-const INSERT = () => Keycap.build('Insert');
-const HOME = () => Keycap.build('Home');
-const PAGE_UP = () => Keycap.build('PgUp');
-const DELETE = () => Keycap.build('Delete');
-const END = () => Keycap.build('End');
-const PAGE_DOWN = () => Keycap.build('PgDn');
+const INSERT = () => Keycap.build('INSERT', 'Insert');
+const HOME = () => Keycap.build('HOME', 'Home');
+const PAGE_UP = () => Keycap.build('PAGE_UP', 'PgUp');
+const DELETE = () => Keycap.build('DELETE', 'Delete');
+const END = () => Keycap.build('END', 'End');
+const PAGE_DOWN = () => Keycap.build('PAGE_DOWN', 'PgDn');
 
-const UP = () => Keycap.build('Up');
-const LEFT = () => Keycap.build('Left');
-const DOWN = () => Keycap.build('Down');
-const RIGHT = () => Keycap.build('Right');
+const UP = () => Keycap.build('UP', 'Up');
+const LEFT = () => Keycap.build('LEFT', 'Left');
+const DOWN = () => Keycap.build('DOWN', 'Down');
+const RIGHT = () => Keycap.build('RIGHT', 'Right');
 
 const section = (...args) => List.of(...args);
 const group = (...args) => List.of(...args);
@@ -122,7 +122,7 @@ const ALPHANUMERIC = section(
     TAB, Q, W, E, R, T, Y, U, I, O, P, LEFT_SQUARE_BRACKET, RIGHT_SQUARE_BRACKET, BACKSLASH,
   ),
   group(
-    CAPSLOCK, A, S, D, F, G, H, J, K, L, SEMI_COLON, SINGLE_QUOTE, ENTER,
+    CAPS_LOCK, A, S, D, F, G, H, J, K, L, SEMI_COLON, SINGLE_QUOTE, ENTER,
   ),
   group(
     LEFT_SHIFT, Z, X, C, V, B, N, M, COMMA, PERIOD, FORWARD_SLASH, RIGHT_SHIFT,
@@ -390,6 +390,7 @@ class Keyboard extends Record({
 
 class Keycap extends Record({
   id,
+  type: "",
   primaryLabel: "",
   secondaryLabel: "",
   width: 1,
@@ -398,6 +399,7 @@ class Keycap extends Record({
   convex: false,
 }) {
   static build(
+    type: string,
     primaryLabel: string = '',
     secondaryLabel: string = '',
     width: number = 1,
@@ -407,6 +409,7 @@ class Keycap extends Record({
   ): Keycap {
     const params = {
       id: id(),
+      type,
       primaryLabel,
       secondaryLabel,
       width,
